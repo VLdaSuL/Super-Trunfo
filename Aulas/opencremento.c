f181fe480b615cedc30d039c64d11bf2c33ffdd3
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 19:23:40 -0300

    atualizações

---
+++ Aulas/opencremento.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+    int main (){
+        /*
+        Incremento (++)
+        Pré-Incremento ++a
+        Pós-Incremento a++
+        Decremento (--)
+        Pré-Decremento --a
+        Pós-Decremento a--
+
+        Esta lição esta no video 3008, não entendi a utilidade
+
+        */
+
+        int numero1 = 1;
+
+        printf("\nAntes incremento: %d\n", numero1);
+        numero1++;
+        printf("Após incremento: %d\n", numero1);
+
+        numero1--;
+        printf("Após decremento: %d\n", numero1);
+
+    return 0;
+
+    }
