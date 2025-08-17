79a7d151448188534a0faf7233b8de9e644c8270
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 13:57:17 -0300

    testando codigo

--- Desafio.c
+++ Desafio.c
@@ -6,7 +6,7 @@
         printf("\nProjeto Jogo Super Trunfo");
 
         //Entranda das variaveis
-        int populacao1, popolacao2, turisco1, turistico2;
+        int populacao1, populacao2, turistico1, turistico2;
         float pib1, pib2, area1, area2;
         char estado1[15], estado2[15], codigo1[5], codigo2[5]; 
         char cidade1[15], cidade2[15];
@@ -28,7 +28,7 @@
         printf("Digite o PIB: \n");
         scanf("%f", &pib1);
         printf("Digite o número de pontos túristicos: \n");
-        scanf("%d", &turisco1);
+        scanf("%d", &turistico1);
 
         //Cadastrando segunda carta
         printf("Cadastrando segunda carta: \n");
@@ -49,6 +49,28 @@
         printf("Digite o número de pontos túristicos: \n");
         scanf("%d", &turistico2);
 
+        //Cartas do jogo
+        printf("\nCartas Cadastradas: \n");
+
+        //Impressão primeira carta
+        printf("\nPrimeira carta: \n");
+        printf("Nome do Estado: %s\n", estado1);
+        printf("Código da carta: %s\n", codigo1);
+        printf("Nome da cidade: %s\n", cidade1);
+        printf("População: %d\n", populacao1);
+        printf("Área em km²: %.3f\n", area1);
+        printf("PIB: %f\nb", pib1);
+        printf("Número de pontos turísticos: %d\n", turistico1);
+
+        //Impressão segunda carta
+        printf("\nPrimeira carta: \n");
+        printf("Nome do Estado: %s\n", estado2);
+        printf("Código da carta: %s\n", codigo2);
+        printf("Nome da cidade: %s\n", cidade2);
+        printf("População: %d\n", populacao2);
+        printf("Área em km²: %.3f\n", area2);
+        printf("PIB: %f\n", pib2);
+        printf("Número de pontos turísticos: %d\n", turistico2);
 
 
         return 0;
