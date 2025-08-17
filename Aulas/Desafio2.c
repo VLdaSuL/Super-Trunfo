dedc654b05199949a8a4581eee0fb1e692bcac78
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 20:56:27 -0300

    teste

--- Aulas/Desafio2.c
+++ Aulas/Desafio2.c
@@ -2,20 +2,23 @@
 
     int main(){
     //Inserindo as variaveis do desfio.
-    float nota1, nota2, soma, media;
-    soma = nota1 + nota2;
+    float nota1, nota2, nota3, soma, media;
+    soma = nota1 + nota2 + nota3;
 
     //Entrada das notas
     printf("\n\nDigite a primeira nota: \n");
     scanf("%f", &nota1);
     printf("Digite a segunda nota: \n");
     scanf("%f", &nota2);
+    printf("Digite a terceira nota: \n");
+    scanf("%f", &nota3);
 
     //Calculando a média
-    media = soma / 2;
+    media = soma / 3;
 
     //Imprimindo resultados
-    printf("\n\nPrimeira nota: %.2f\n", nota1);
+    printf("\n\nNOTAS: \n")
+    printf("Primeira nota: %.2f\n", nota1);
     printf("Segunda nota: %.2f\n", nota2);
     printf("Média final: %.2f\n", media);
 
