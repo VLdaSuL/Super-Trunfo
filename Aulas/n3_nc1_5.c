02d921f96b9de842754ef4a123e71995441be041
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 23:41:55 -0300

    short int

---
+++ Aulas/n3_nc1_5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main(){
+        short int numeroPequeno = 32767; //Valor máximo de short int
+        printf ("Número pequeno (short int): %d\n", numeroPequeno);
+
+        numeroPequeno = 32768; //Valor maior que o máximo de short int
+        printf("Número pequeno atualizado (short int): %d\n", numeroPequeno);
+
+    return 0;
+
+    }
