248bc50f6260ef7e0fdebc66fd14b13d1492a5fe
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 23:26:04 -0300

    unsigned long int

--- Aulas/n3_nc1_3.c
+++ Aulas/n3_nc1_3.c
@@ -2,11 +2,11 @@
 
     int main(){
         int numeroNormal = 2147483647;
-        long int numeroGrande = 2147483647;
+        long long int numeroGrande = 2147483648;
         //Valor máximo de int
 
         printf("\n\nNúmero regular (int): %d\n", numeroNormal);
-        printf("Número grande (long int): %ld\n", numeroGrande);
+        printf("Número grande (long long int): %lld\n", numeroGrande);
 
     return 0;
 
