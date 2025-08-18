b5b353015ecf78b3a72c8969ea9b72bc65d8f3c3
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 23:46:04 -0300

    aula ok

--- Aulas/n3_nc1_5.c
+++ Aulas/n3_nc1_5.c
@@ -2,7 +2,7 @@
 
     int main(){
         short int numeroPequeno = 32767; //Valor máximo de short int
-        printf ("Número pequeno (short int): %d\n", numeroPequeno);
+        printf ("\n\nNúmero pequeno (short int): %d\n", numeroPequeno);
 
         numeroPequeno = 32768; //Valor maior que o máximo de short int
         printf("Número pequeno atualizado (short int): %d\n", numeroPequeno);
