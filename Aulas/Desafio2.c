df7f66cd6b0edd641f4c2b9c8223807a957129b1
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 21:03:23 -0300

    correções

--- Aulas/Desafio2.c
+++ Aulas/Desafio2.c
@@ -3,7 +3,7 @@
     int main(){
     //Inserindo as variaveis do desfio.
     float nota1, nota2, nota3, soma, media;
-    soma = nota1 + nota2 + nota3;
+    soma = (nota1 + nota2 + nota3);
 
     //Entrada das notas
     printf("\n\nDigite a primeira nota: \n");
@@ -14,12 +14,13 @@
     scanf("%f", &nota3);
 
     //Calculando a média
-    media = soma / 3;
+    media = (soma / 3);
 
     //Imprimindo resultados
-    printf("\n\nNOTAS: \n")
+    printf("\n\nNOTAS: \n");
     printf("Primeira nota: %.2f\n", nota1);
     printf("Segunda nota: %.2f\n", nota2);
+    printf("Terceira nota: %.2f\n", nota3);
     printf("Média final: %.2f\n", media);
 
     return 0;
