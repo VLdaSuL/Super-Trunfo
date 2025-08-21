054d2fb045efc1662215205eed22892c68a26c28
Marcel Santos <vldasul@yahoo.com.br>
2025-08-20 21:49:29 -0300

    código testado e comentado

---
+++ Desenvolvimento/tema3_vd1214.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+    int main (){
+        /*
+        if  (condicao) {
+            //bloco de código a ser executado se a condição for verdadeira
+            }
+        else {
+            // bloco de código a ser executado se a condição for falsa
+            }
+            */
+        /*
+        int numero1, numero2;
+
+        numero1 = 5000;
+        numero2 = 2340;
+
+        if (numero1 > numero2) {
+            printf("\n\nNumero 1 é maior que numero 2\n");
+        }else {
+                printf("\n\nNumero 2 é maior que o numero 1\n\n");
+            }
+        */
+        float temperatura;
+
+        temperatura = 9.00;
+
+        if (temperatura > 25.00) {
+            printf("\n\nSim, temperatura esta ok\n\n");
+        }
+        else {
+            printf("\n\nNão, temperatura esta baixa\n\n");
+        }
+
+    }
