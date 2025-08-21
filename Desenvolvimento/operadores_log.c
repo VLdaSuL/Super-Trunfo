c567703091332d5c74a556443f1031b276ff102e
Marcel Santos <vldasul@yahoo.com.br>
2025-08-20 23:21:19 -0300

    testando operadores lógicos

---
+++ Desenvolvimento/operadores_log.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    int main (){
+    
+    int A, B;
+    A = 20;
+    B = 40;
+
+
+    //Testando a lógica and &&
+    if (A > 0 && B > 0)
+{
+    printf("\n\nOk, ambos os números são positivos!\n");
+} else {
+    printf("\n\nOpa, um número é negativo\n");
+}
+
+}
+
+
+
+
+
