c1b11cdb3875eb38c83eff029104cc4e0372526c
Marcel Santos <vldasul@yahoo.com.br>
2025-08-21 01:47:38 -0300

    criando projeto logica aninhada parte dois

---
+++ Desenvolvimento/logica_aninhada2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+    int main(){
+
+        int idade, dependentes;
+        float renda;
+    
+    // A primeira condição idade do usuário entre 18 e 65 anos.
+    // A segunda condição se a renda do usuário é menor que 3000.
+    // A terceira condição verificar se o número de dependentes é maior que 2.
+
+    //Entrada dos dados do usuário:    
+    printf("\n\nDigite a sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite a tua renda: \n");
+    scanf("%f", &renda);
+    printf("Digite o número de dependentes: \n");
+    scanf("%d", &dependentes);
+
+    //Estrutura de filtragem:
+    if (idade < 18 || idade > 65) {}
+
+
+    }
