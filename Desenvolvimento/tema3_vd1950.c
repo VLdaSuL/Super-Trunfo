d66036224d50143fc67ad776eaacf3ba64a5efc4
Marcel Santos <vldasul@yahoo.com.br>
2025-08-20 21:25:15 -0300

    finalizado

--- Desenvolvimento/tema3_vd1950.c
+++ Desenvolvimento/tema3_vd1950.c
@@ -6,10 +6,10 @@
 
         resultado = numero % 2;
         //Teste simples para verificar numero par ou impar
-        printf("A variável resultado é: %d\n", resultado);
+        printf("\n\n\nA variável resultado é: %d\n", resultado);
         //Este comando imprime na tela o resto da variavel
         if (resultado == 0) {
-            printf ("O número é par\n");
+            printf ("O número é par\n\n\n");
         }
         //Caso o comando if seja correspondido, a mensagem dentro das chaves será impressa
     return 0;
