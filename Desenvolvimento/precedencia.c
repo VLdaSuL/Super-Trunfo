5ca5148de86054410d46cfbee6e06b527a138c17
Marcel Santos <vldasul@yahoo.com.br>
2025-08-21 00:16:53 -0300

    codigo finalizado

--- Desenvolvimento/precedencia.c
+++ Desenvolvimento/precedencia.c
@@ -18,7 +18,7 @@ ordem de cima para baixo e da esquerda para direita
 video de instrução tema03 video2209
 
 */
-
+/*
 int a = -5;
 int b = 10;
 int c = 0;
@@ -28,5 +28,19 @@ if (a > 0 && b < 0 || c == 0) {
 } else {
     printf("\n\nAlgo deu errado\n\n");
 }
+*/
+
+int idade;
+float altura;
+
+idade = 31;
+altura = 1.71;
+
+if (idade >= 18 && idade <= 30 && altura > 1.70) {
+    printf("\n\nCandidato aprovado\n\n");
+} else {
+    printf("\n\nCandidato reprovado\n\n");
+}
+
 
 }
