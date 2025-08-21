4ce79885e82bf7137d6c5b67ed3120eeec6b784d
Marcel Santos <vldasul@yahoo.com.br>
2025-08-20 21:22:45 -0300

    Código Ok

--- Desenvolvimento/tema3_vd1950.c
+++ Desenvolvimento/tema3_vd1950.c
@@ -2,16 +2,16 @@
 
     int main (){
 
-        int numero = 5, resultado;
+        int numero = 8888, resultado;
 
         resultado = numero % 2;
-
+        //Teste simples para verificar numero par ou impar
         printf("A variável resultado é: %d\n", resultado);
-
+        //Este comando imprime na tela o resto da variavel
         if (resultado == 0) {
             printf ("O número é par\n");
         }
-
+        //Caso o comando if seja correspondido, a mensagem dentro das chaves será impressa
     return 0;
 
     }
