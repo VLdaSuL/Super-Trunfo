c69543f0588cf77a916be2e5cba225eced4c7c41
Marcel Santos <vldasul@yahoo.com.br>
2025-08-22 19:51:28 -0300

    projeto finalizado

--- Desenvolvimento/projeto_t3_operadores.c
+++ Desenvolvimento/projeto_t3_operadores.c
@@ -1,8 +1,8 @@
-#include <stdio>
+#include <stdio.h>
 
     int main(){
         int opcao;
-        int nota1, nota2, media;
+        float nota1, nota2, media;
 
         printf("Menu de Gerenciamento de Estudantes\n");
         printf("1. Calcular Média\n");
@@ -11,7 +11,44 @@
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
+        switch (opcao) {
+            //Primeira parte, entrada das notas
+            case 1:
+            printf("\nCalcular a Média\n");
+            printf("\nDigite a primeira nota:\n");
+            scanf("%f", &nota1 );
+            printf("\nDigite a segunda nota:\n");
+            scanf("%f", &nota2 );
+            //Teste de condição se a nota é >= 0 e <= 10
+            if ( (nota1 >= 0 && nota1 <=10) && (nota2 >= 0 && nota2 <=10) ) {
+                media = (nota1 + nota2) / 2;
+                printf ("\nA média é: %.2f\n\n", media);
+            //printf("Entrada correta");                
+            } else {
+                printf("Digite corretamente os valores das notas!\n\n");
+            }          
+            break;
+            //Segunda parte, determinar se aluno foi aprovado ou reprovado
+            case 2:
+                printf("\nDeterminar status\n");
+                printf("Entrar com o valor da média do aluno: ");
+                scanf("%f", &media);
+                //media >= 6 ? printf("Aluno esta aprovado!\n\n") : printf("Aluno esta reprovado.\n\n");
+                //Acima usando ternário e abaixo usando if e else
+                if (media >= 6) {
+                    printf("Aluno esta aprovado\n\n");
+                } else if (media >= 5) {
+                    printf("Aluno esta de recuperação!\n\n");
+                } else {
+                    printf("Aluno esta reprovado!\n\n");
+                }
+            break;
+            default:
+                    printf("\nFim do programa\n\n\n");
+            break;    
+        }
 
 
+    return 0;
 
     }
