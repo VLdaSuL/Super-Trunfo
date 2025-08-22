f6ec8a2ce0329887d709f5d4788f94dc721b31fd
Marcel Santos <vldasul@yahoo.com.br>
2025-08-21 22:09:27 -0300

    cógigo testado

--- Desenvolvimento/switch_01.c
+++ Desenvolvimento/switch_01.c
@@ -5,15 +5,17 @@
 int variavel;
 
 printf("\n\nDigite um valor\n");
+scanf("%d", &variavel);
 
 switch (variavel) {
-case valor1:
-//codigo a ser executado se variavel == valor1
+case 1:
+    printf("codigo a ser executado se variavel == valor1");
 break;
-case valor2
-
+case 2:
+    printf("Código a ser executado se variavel == valor2");
+break;
 default:
-    break;
+    printf("Código a ser executado se variavel não for 1 ou 2\n");
 }
 
-    }
+}
