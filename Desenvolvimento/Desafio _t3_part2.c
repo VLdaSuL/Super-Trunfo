969bcf6b9c26ea1ae652fe32dedab5a605efd125
Marcel Santos <vldasul@yahoo.com.br>
2025-08-22 17:18:23 -0300

    Projeto Super Trunfo menu interativo finalizado

--- Desenvolvimento/Desafio _t3_part2.c
+++ Desenvolvimento/Desafio _t3_part2.c
@@ -1,23 +1,238 @@
 #include <stdio.h>
 
-    int main(){
+    int main() {
 
-        int opcao;
+        //Entranda das variaveis;
+        int turistico1, turistico2;
+        int populacao1, populacao2;
+        float pib1, pib2, area1, area2;
+        double densidade1, densidade2, percapita1, percapita2;
+        double super1, super2;
+        char estado1[15], estado2[15], codigo1[5], codigo2[5]; 
+        char cidade1[15], cidade2[15];
+        int opcao, batalha;
 
     printf("\n\nJOGO SUPER TRUNFO\n\n");
     printf("1. Regras do jogo\n");
     printf("2. Inicio do jogo\n");
-    printf("3. Resultado do jogo\n");
-    printf("4. Sair do jogo\n");
+    printf("3. Sair do jogo\n");
 
     printf("\n\nDigite uma opção:");
     scanf("%d", &opcao);
 
     switch (opcao) {
-    case 1.
-    printf("Neste jogo, ao clicar na opção 2, ");
-    break;
+        case 1:
+        printf("\nEste é o jogo Super Trunfo\n\n");
+        printf("Você pode jogar sozinho, mais com um amigo, fica ainda mais divertido.\n");
+        printf("Cada jogador vai precisar de 1 carta do jogo Super Trunfo.\n");
+        printf("No inicio do jogo, o jogador 1 vai inserir os atributos de sua carta.\n");
+        printf("Na sequência, é a vez do jogar 2 inserir os atributos da sua carta.\n");
+        printf("No menu a seguir, só escolher um dos atributos e torcer!.\n");
+        printf("A carta vencedora será aprensentada e o resultado final da batalha.\n");
+        printf("Para continuar, só trocarem as cartas por uma nova e reiniciar o jogo.\n");
+        printf("Boa Jogo!!!.\n\n");
+        printf("Volte ao menu inicial.\n\n");
+        break;
+
+        case 2:
+        //Cadastrando primeira carta;
+        printf("Digite os dados da primeira carta: \n");
+        //Entrada dos dados;
+        printf("Digite o nome do Estado: \n");
+        scanf("%s", estado1);
+        printf("Digite o código da carta: \n");
+        scanf("%s", codigo1);
+        printf("Digite o nome da cidade: \n");
+        scanf("%s", cidade1);
+        printf("Digite a população: \n");
+        scanf("%d", &populacao1);
+        printf("Digite a área em km²: \n");
+        scanf("%f", &area1);
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib1);
+        printf("Digite o número de pontos túristicos: \n");
+        scanf("%d", &turistico1);
+        //Cadastrando segunda carta;
+        printf("Digite os dados da segunda carta: \n");
+        //Entrada dos dados;
+        printf("Digite o nome do Estado: \n");
+        scanf("%s", estado2);
+        printf("Digite o código da carta: \n");
+        scanf("%s", codigo2);
+        printf("Digite o nome da cidade: \n");
+        scanf("%s", cidade2);
+        printf("Digite a população: \n");
+        scanf("%d", &populacao2);
+        printf("Digite a área em km²: \n");
+        scanf("%f", &area2);
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib2);
+        printf("Digite o número de pontos túristicos: \n");
+        scanf("%d", &turistico2);
+        //Calculando váriavel densidade populacional;
+        densidade1 = (float)populacao1 / area1;
+        densidade2 = (float)populacao2 / area2;
+        //Calculando variável PIB per Capita;
+        //Conversão do PIB em bilhões;
+        percapita1 = (pib1*1000000000) / (float)populacao1;
+        percapita2 = (pib2*1000000000) / (float)populacao2;
+        //Calculando super poderes
+        super1 = (float)(populacao1 + area1 + pib1 + turistico1 + percapita1);
+        super2 = (float)(populacao2 + area2 + pib2 + turistico2 + percapita2);        
+
+        /*        
+        //Cartas do jogo;
+        printf("\nVeja agora, as duas cartas que você cadastrou:\n");
+        //Impressão primeira carta;
+        printf("\nCARTA 1\n");
+        printf("Nome do Estado: %s\n", estado1);
+        printf("Código da carta: %s\n", codigo1);
+        printf("Nome da cidade: %s\n", cidade1);
+        printf("População: %d habitantes\n", populacao1);
+        printf("Área: %.3f km²\n", area1);
+        printf("PIB: %.2f bilhões de reais\n", pib1);
+        printf("Número de pontos turísticos: %d\n", turistico1);
+        printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+        printf("PIB per Capita: %.2f reais\n", percapita1);
+        //Impressão segunda carta
+        printf("\nCARTA 2\n");
+        printf("Nome do Estado: %s\n", estado2);
+        printf("Código da carta: %s\n", codigo2);
+        printf("Nome da cidade: %s\n", cidade2);
+        printf("População: %d habitantes\n", populacao2);
+        printf("Área: %.2f km²\n", area2);
+        printf("PIB: %.2f bilhões de reais\n", pib2);
+        printf("Número de pontos turísticos: %d\n", turistico2);
+        printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+        printf("PIB per Capita: %.2f reais\n", percapita2);
+        */    
+
+        //Jogador escolhe atributo e verifica resultado da batalha
+        printf("\n\nEscolha um atributo, e veja carta vencedora:\n\n");
+        printf("1. População\n");
+        printf("2. Área em km²\n");
+        printf("3. PIB\n");
+        printf("4. Número de pontos turísticos\n");
+        printf("5. Densidade Populacional\n");
+        printf("6. PIB per Capita\n");
+        printf("7. Super Poder\n"); 
+        
+        printf("\nEscolha uma batalha: \n");
+        scanf("%d", &batalha);
+
+            switch (batalha) {
+            case 1:
+            if (populacao1 > populacao2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (populacao1 < populacao2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("População da Carta 1: %d habitantes.\n", populacao1);
+                printf("População da Carta 2: %d habitantes.\n\n\n", populacao2);
+                printf("\nFIM DE JOGO!!!\n");                
+            break;
+
+            case 2:
+            if (area1 > area2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (area1 < area2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("Area da Carta 1: %.2f km².\n", area1);
+                printf("Área da Carta 2: %.2f km².\n\n\n", area2);
+                printf("\nFIM DE JOGO!!!\n");                
+            break;
+
+            case 3:
+            if (pib1 > pib2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (pib1 < pib2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("PIB da Carta 1: %.2f Bilhões de reais.\n", pib1);
+                printf("PIB da Carta 2: %.2f Bilhões de reais.\n\n\n", pib2);
+                printf("\nFIM DE JOGO!!!\n");                
+            break; 
+
+            case 4:
+            if (turistico1 > turistico2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (turistico1 < turistico2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("Pontos Turísticos da Carta 1: %d .\n", turistico1);
+                printf("Pontos Turísticos da Carta 2: %d .\n\n\n", turistico2);
+                printf("\nFIM DE JOGO!!!\n");                
+            break; 
+
+            case 5:
+            if (densidade1 < densidade2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (densidade1 > densidade2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("Densidade Populacional da Carta 1: %.2f habitantes por km².\n", densidade1);
+                printf("Densidade Populacional da Carta 2: %.2f habitantes por km².\n\n\n", densidade2);
+                printf("\nFIM DE JOGO!!!\n");
+            break;
+
+            case 6:
+            if (percapita1 > percapita2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (percapita1 < percapita2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("PIB per Capita da Carta 1: %.2f reais.\n", percapita1);
+                printf("PIB per Capita da Carta 2: %.2f reais.\n\n\n", percapita2);
+                printf("\nFIM DE JOGO!!!\n");
+            break;
+
+            case 7:
+            if (super1 > super2) {
+                printf("\nCarta 1 venceu!\n");
+            } else if (super1< super2) {
+                printf("\nCarta 2 venceu!\n");  
+            } else {
+                printf("\nIncrivel, houve um empate!!!\n");
+            } 
+                printf("\nResultado desta batalha:\n");
+                printf("Super Poder da Carta 1: %.2f pontos.\n", super1);
+                printf("Super Poder da Carta 2: %.2f pontos.\n\n\n", super2);
+                printf("\nFIM DE JOGO!!!\n");
+            break;
+
+            default:
+                printf("\n\nOpção inválida!\n");
+                printf("\nFIM DE JOGO!!!\n\n");
+            break; 
+        }                       
+        break;
+        
+        case 3:
+            printf("\n\nFIM DE JOGO!!!\n\n");            
+        break;
+
     default:
+        printf("\n\nOpção inválida!\n");
+        printf("\nFIM DE JOGO!!!\n\n");
     break;
     }
 
