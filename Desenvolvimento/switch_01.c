658eac034099ba33c55dd88960bc7fa2869cdceb
Marcel Santos <vldasul@yahoo.com.br>
2025-08-21 21:52:35 -0300

    aprendendo comando switch

---
+++ Desenvolvimento/switch_01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+    int main (){
+
+int variavel;
+
+printf("\n\nDigite um valor\n");
+
+switch (variavel) {
+case valor1:
+//codigo a ser executado se variavel == valor1
+break;
+case valor2
+
+default:
+    break;
+}
+
+    }
