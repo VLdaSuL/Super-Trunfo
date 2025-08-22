3dbd871945432a8fc3bcf11a2350fcb49831ccc4
Marcel Santos <vldasul@yahoo.com.br>
2025-08-22 18:22:43 -0300

    projeto om operadores switch if else ternário.

---
+++ Desenvolvimento/projeto_t3_operadores.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+    int main(){
+        int opcao;
+        int nota1, nota2, media;
+
+        printf("Menu de Gerenciamento de Estudantes\n");
+        printf("1. Calcular Média\n");
+        printf("2. Determinar Status\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+
+
+
+    }
