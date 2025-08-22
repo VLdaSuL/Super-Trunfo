d4e479f2bbbdd54afe02979288950d7a10b0c96b
Marcel Santos <vldasul@yahoo.com.br>
2025-08-22 12:43:47 -0300

    projeto super trunfo

---
+++ Desenvolvimento/Desafio _t3_part2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+    int main(){
+
+        int opcao;
+
+    printf("\n\nJOGO SUPER TRUNFO\n\n");
+    printf("1. Regras do jogo\n");
+    printf("2. Inicio do jogo\n");
+    printf("3. Resultado do jogo\n");
+    printf("4. Sair do jogo\n");
+
+    printf("\n\nDigite uma opção:");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+    case 1.
+    printf("Neste jogo, ao clicar na opção 2, ");
+    break;
+    default:
+    break;
+    }
+
+return 0;
+
+}
