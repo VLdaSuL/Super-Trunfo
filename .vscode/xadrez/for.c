63f56f1ce981b7508eef15dae99607beb42d73e7
Marcel Santos <vldasul@yahoo.com.br>
2025-08-23 23:22:24 -0300

    testado

--- .vscode/xadrez/for.c
+++ .vscode/xadrez/for.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
     int main (){
         // função - inicialização  - condição - incremento
-        for(int i = 1; i < 5; i++) {
+        for(int i = 1; i <= 5; i++) {
             printf("%d\n", i);
             printf("Jatobá\n"); //testando este código
         }
