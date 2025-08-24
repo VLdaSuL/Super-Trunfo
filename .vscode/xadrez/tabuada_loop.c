08206562d281cc3c5724b44b404501fa72671fe1
Marcel Santos <vldasul@yahoo.com.br>
2025-08-24 20:08:32 -0300

    entendendo loop aninhado fazendo tabuada

---
+++ .vscode/xadrez/tabuada_loop.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+    int main(){
+
+        printf("Tabuada com loop while... ")
+
+            int i = 1;
+            while (i <= 10)
+            {
+                
+                int j = 1
+                while (j <= i)
+                {
+                   printf("%d\t", i * j);
+                }                                        
+                j++;
+
+
+            }
+            printf("\n");
+            i++
+            
+
+
+
+
+    return 0;
+
+    }
