4fb674cb8b3fd27ef9267bede2714484a8b8e5f8
Marcel Santos <vldasul@yahoo.com.br>
2025-08-23 23:11:01 -0300

    teste do código ok

--- .vscode/xadrez/do_while.c
+++ .vscode/xadrez/do_while.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
     int main (){
         int i = 1; //Variavel de incremento
+            
+            printf("\n");
 
         do { //Entrada (1x pelo menos)
             printf("%d\n", i); // Saida aula
