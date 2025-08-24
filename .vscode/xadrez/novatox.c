7caebc93dea3f63abc6bc71762882a6bf62e9cf4
Marcel Santos <vldasul@yahoo.com.br>
2025-08-24 15:31:22 -0300

    Desenvolvendo a tarefa

--- .vscode/xadrez/novatox.c
+++ .vscode/xadrez/novatox.c
@@ -7,7 +7,15 @@
         //Torre 5 casas para frente
 
         int jtorre, jbispo, jrainha;
-        int numero, i;
+        int numero, i, jogadas;
+
+        printf("\n");
+        printf("Jogo de Xadrez: \n");
+        printf("1. Informações\n");
+        printf("2. Movimentando o Bispo\n");
+        printf("3. Movimentando a Rainha\n");
+        printf("4. Movimenando a Torre\n");
+        printf("5. Sair\n");
 
 
 
