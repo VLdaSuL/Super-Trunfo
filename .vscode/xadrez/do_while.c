e33483681a99ace2ad13ba949f9653b17029dbfc
Marcel Santos <vldasul@yahoo.com.br>
2025-08-23 22:58:37 -0300

    testando do while

---
+++ .vscode/xadrez/do_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    int main (){
+        int i = 1; //Variavel de incremento
+
+        do { //Entrada (1x pelo menos)
+            printf("%d\n", i); // Saida aula
+            printf("Jurupinga\n"); // Saida teste 
+            i++; //incremento
+        } while (i <= 5); //condição
+
+
+
+    return 0;
+
+    }
