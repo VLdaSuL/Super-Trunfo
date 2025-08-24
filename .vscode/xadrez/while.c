83141ed76ee618cf0943385aab7b83c27bd9982d
Marcel Santos <vldasul@yahoo.com.br>
2025-08-23 22:19:18 -0300

    testando comando while

---
+++ .vscode/xadrez/while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main (){
+        int bispo = 1;
+
+        while (bispo <- 8) {
+            printf("%d\n", bispo);
+            bispo++;
+        }
+
+
+
+return 0;
+
+    }
