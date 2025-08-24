96ae5438232f67705304fd6f1a4ffe4e1bf58d05
Marcel Santos <vldasul@yahoo.com.br>
2025-08-23 23:27:45 -0300

    Aula prática

---
+++ .vscode/xadrez/pratica_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    int main (){
+        int i =0;
+
+        while(i < 10) {
+            if (1 %2 == 0) {
+                printf("O numero %d é par!\n", i);
+            }
+            i++;
+        }
+
+    return 0;
+
+    }
