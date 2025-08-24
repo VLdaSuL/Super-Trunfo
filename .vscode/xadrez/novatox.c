aea8ef615127b348a3a09d1f686a864f6e47fe2a
Marcel Santos <vldasul@yahoo.com.br>
2025-08-24 00:23:45 -0300

    iniciando desafio

---
+++ .vscode/xadrez/novatox.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+    int main () {
+
+        //Desafio:
+        //Bispo 5 casas na diagonal
+        //Rainha 8 casas para esquerda
+        //Torre 5 casas para frente
+
+        int jtorre, jbispo, jrainha;
+        int numero, i;
+
+
+
+
+
+
+
+    return 0;
+
+    }
+
+    
