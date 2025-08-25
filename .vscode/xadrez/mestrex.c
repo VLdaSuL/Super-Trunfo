c18d1bb24b1287bab495c7d14c6187cfd90a3a22
Marcel Santos <vldasul@yahoo.com.br>
2025-08-25 17:22:45 -0300

    Desafio concluido

--- .vscode/xadrez/mestrex.c
+++ .vscode/xadrez/mestrex.c
@@ -1,13 +1,62 @@
 #include<stdio.h>
 
+    //Desafio Mestre Prof Sergio Cardoso.
+    //Mover o Bispo 5x diagonal para cima - Cima/Direita
+    //Mover a Torre 5x para Direita
+    //Mover a Rainha 8x para Esquerda
+    //Mover o Cavalo 2x para Cima e Direita
+
+    void moverBispo (int casas) {
+        if (casas > 0) {
+        printf("Direita\n");
+        printf("Cima\n");        
+        moverBispo (casas -1);
+        }
+    }
     void moverTorre (int casas) {
+        if (casas > 0) {
         printf("Direita\n");
         moverTorre (casas -1);
+        }
+    }
+    void moverRainha (int casas) {
+        if (casas > 0) {
+        printf("Esquerda\n");
+        moverRainha (casas -1);
+        }
+    }
+    void moverCavalo (int casas) {
+        if (casas > 0) {
+        printf("Cima\n");
+        moverCavalo (casas -1);
+        }
     }
 
-    int main() {
+        int main() {
+        int jogada;
+        //Nome do projeto
+        printf("\n\n");
+        printf("Movimentando as peças de xadrez:\n");        
+        printf("\n");
+        //Movimento do Bispo
+        printf("Movimentando o Bispo.\n");
+        moverBispo (5);
+        printf("\n");
+        //Moviemento da Torre
+        printf("Movimentando a Torre.\n");
+        moverTorre (5);
+        printf("\n");
+        //Moviemento da Rainha
+        printf("Movimentando a Rainha.\n");
+        moverRainha (8);
+        printf("\n");
+        //Movimento do Cavalo      
+        printf("Movimentando a Cavalo.\n");
+        moverCavalo (2);
+        printf("Direita\n");
+        printf("\n");
 
-        moverTorre (5)
+
         return 0;
 
     }
