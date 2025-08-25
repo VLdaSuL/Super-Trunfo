58ec11eef4092a678b3ff19105cef66d23836bb8
Marcel Santos <vldasul@yahoo.com.br>
2025-08-24 21:30:16 -0300

    projeto finalizado

--- .vscode/xadrez/tabuada_loop.c
+++ .vscode/xadrez/tabuada_loop.c
@@ -1,26 +1,67 @@
 #include <stdio.h>
     int main(){
 
-        printf("Tabuada com loop while... ")
+ /*       //Tabuada
+        int numero, i;
+        //printf(:"\n");
+        printf("Digite um numero para calcularmos a tabuada:   ");
+        scanf("%d", &numero);
+        //printf("\n");
+        for(i = 0; i <=10; i++) 
+        {
+            printf("%d x %d = %d \n", i, numero, i * numero);
+        }
+*/        //printf("\n");
 
-            int i = 1;
-            while (i <= 10)
+
+/*
+        printf("\n");
+        printf("Tabuada:  \n");
+        int i = 1;
+        while (i <= 1)
+        {
+            int j = 0;
+            while (j <= 10)
             {
-                
-                int j = 1
-                while (j <= i)
-                {
-                   printf("%d\t", i * j);
-                }                                        
-                j++;
+             printf("%d x %d = %d \n", i, j, i * j);
+             j++;               
+            }     
+        printf("\n");
+        i++;
+        }
+*/
 
-
-            }
+/*        printf("\n");
+        printf("Tabuada:  \n");
+        int i = 1;
+        do
+        {
+            do
+            {
+            int j = 0; //Variavel do DO interno
             printf("\n");
-            i++
-            
+            printf("%d x %d = %d \n", i, j, i * j);
+            j++;
+            } while (j <= 10);
+        
+        printf("\n");
+        i++;
+        } while (i <= 4);
+ */       
 
+        printf("\n");
+        printf("Tabuada:  \n");
+        for (int i = 1; i <= 3; i++)
+        {
 
+          for (int j = 0; j <=10; j++)
+          {
+            printf("%d x %d = %d\n", i, j, i * j);           
+          }
+        printf("\n");         
+        }
+     
+        //Tabuada com while, do while e for
 
 
     return 0;
