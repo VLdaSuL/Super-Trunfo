4389f3b56c76924bc25e2dc351c72f22eba56105
Marcel Santos <vldasul@yahoo.com.br>
2025-08-24 23:43:28 -0300

    movimentos codificados

--- .vscode/xadrez/switc_cavalo.c
+++ .vscode/xadrez/switc_cavalo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
     int main (){
         int cavalo;
+        int i = 1;
         printf("\n");
         printf("\nMovimentando Cavalo:\n");
         printf("1. Cima e Direita\n");
@@ -13,7 +14,7 @@
         printf("8. Cima e Esquerda\n");        
         printf("9. Sair.\n");
         printf("\n");      
-        printf("Escolha um dos oito movimentos do Cavalo: ");
+        printf("Escolha um movimentos para o Cavalo: ");
         scanf("%d", &cavalo);
 
         switch (cavalo)
@@ -21,7 +22,6 @@
         case 1:
             //Cima e para Direita
             printf("\nMovimentando o Cavalo. \n");
-            int i = 1;
             do 
             {
             int j;
@@ -30,41 +30,106 @@
             }            
             printf("Direita\n");
             i++;
-            } while (i <= 1);
+            } while ( i <= 1);          
             printf("\n");
         break;
          case 2:
             //Direita e para Cima
-            printf("\nMovimentando o Cavalo. \n");
-            int i = 1;
-            do 
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
             {
             int j;
             for (j = 1; j <= 2; j++) {
+            printf("Direita\n");
+            }            
             printf("Cima\n");
+            i++;
+            } while (i <= 1);
+            printf("\n");
+        break;
+        case 3:        
+            //Direita e para Baixo
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
+            {
+            int j;
+            for (j = 1; j <= 2; j++) {
+            printf("Direita\n");
+            }            
+            printf("Baixo\n");
+            i++;
+            } while (i <= 1);
+            printf("\n");               
+        break;        
+        case 4:
+            //Baixo e para Direita
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
+            {
+            int j;
+            for (j = 1; j <= 2; j++) {
+            printf("Baixo\n");
             }            
             printf("Direita\n");
             i++;
             } while (i <= 1);
             printf("\n");
         break;
-        case 3:
-            /* code */
-        break;        
-        case 4:
-            /* code */
-        break;
         case 5:
-            /* code */
+            //Baixo e para Esquerda
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
+            {
+            int j;
+            for (j = 1; j <= 2; j++) {
+            printf("Baixo\n");
+            }            
+            printf("Esquerda\n");
+            i++;
+            } while (i <= 1);
+            printf("\n");
         break;
         case 6:
-            /* code */
+            //Esquerda e para Baixo
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
+            {
+            int j;
+            for (j = 1; j <= 2; j++) {
+            printf("Esquerda\n");
+            }            
+            printf("Baixo\n");
+            i++;
+            } while (i <= 1);
+            printf("\n");
         break;
         case 7:
-            /* code */
+            //Esquerda e para Cima
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
+            {
+            int j;
+            for (j = 1; j <= 2; j++) {
+            printf("Esquerda\n");
+            }            
+            printf("Cima\n");
+            i++;
+            } while (i <= 1);
+            printf("\n");
         break;
         case 8:
-            /* code */
+            //Cima e para Esquerda
+            printf("\nMovimentando o Cavalo. \n");          
+            do     
+            {
+            int j;
+            for (j = 1; j <= 2; j++) {
+            printf("Cima\n");
+            }            
+            printf("Esqueda\n");
+            i++;
+            } while (i <= 1);
+            printf("\n");
         break;
         case 9:
             printf("Saindo.");
