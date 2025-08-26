8c13a1aa0cf6657c4762664e9a1e782a2fd2b972
Marcel Santos <vldasul@yahoo.com.br>
2025-08-25 21:00:48 -0300

    ok

--- .vscode/BatalhaNaval/ArrayStrings.c
+++ .vscode/BatalhaNaval/ArrayStrings.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main (){
+
+    char *nomes [] = {"Pyetro","Dianna","Alanna"};
+
+    for (int i = 0; i < 3; i++) 
+   
+    {
+        printf("%s \n", nomes[i]);
+    }
+
+
+
+return 0;
+
+}
