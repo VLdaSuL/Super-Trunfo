d12b8a10192917e8568831728592102264facabd
Marcel Santos <vldasul@yahoo.com.br>
2025-08-27 13:47:27 -0300

    código ok

---
+++ .vscode/BatalhaNaval/soma2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+    int main(){
+        int mat1 [3][3] = {{1, 1, 1}, {2, 2, 2}, {3, 3,3}};
+        int mat2 [3][3] = {{4, 4, 4}, {5, 5, 5}, {6, 6, 6}};
+        int matSoma [3][3];
+
+        printf("\n");
+        //Somando as duas matrizes
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matSoma[i][j] = mat1[i][j] + mat2[i][j];
+                
+                printf("matSoma[%d][%d] =  %d\n", i, j, matSoma[i][j]);                
+            }
+            printf("\n");
+        }
+        
+
+return 0;
+
+    }
