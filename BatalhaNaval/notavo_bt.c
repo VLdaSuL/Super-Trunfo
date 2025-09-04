26c45db5e28e60e00cfae7dfdaefd0544b9765b9
Marcel Santos <vldasul@yahoo.com.br>
2025-09-04 12:36:17 -0300

    Tabuleiro finalizado

--- BatalhaNaval/notavo_bt.c
+++ BatalhaNaval/notavo_bt.c
@@ -1,24 +1,38 @@
 #include<stdio.h>
+
     int main(){
 
         int i, j, l;
 
-        int tabuleiro[10][10] = {};
+        int tabuleiro[10][10] = {
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {1,2,3,4,5,6,7,8,9,0},
+        {8,2,3,4,5,6,7,8,9,0} };
+
+        char x [11] = {'X','A','B','C','D','E','F','G','H','I','J'};
+
+        int y [10] = {1,2,3,4,5,6,7,8,9,10};
 
         printf("\n\n");
         printf("TABULEIRO BATALHA NAVAL \n");
         printf("\n");
 
-        char letra[10] = {'A','B','C','D','E','F','G','H','I'};
-        for (int l = 0; l < 10; l++) 
+        for (int l = 0; l <= 10; l++) 
         {
-            printf (" %c", letra[l]); 
+            printf (" %c", x [l]); 
         }   printf("\n");
 
         for (int i = 0; i < 10; i++) 
         {
            
-            printf("%d", i);  //AJUSTE
+            printf("%2d", y [i]);  //AJUSTE
             
             for (int j = 0; j < 10; j++)          
             {            
